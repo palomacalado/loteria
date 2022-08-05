fa5b95ae4e814ca07d73c7af5b0cb36343f00905
Paloma Calado <palomacalado@dcc.ufrj.br>
2022-08-04 22:31:50 -0300

    :sparkles: Chamadas de componentes
    chamando componentes criados no App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Ball from './components/ball/ball';
+import Selector from './components/selector/selector';
+
+const Background = styled.div`
+  background-color: #6befa3;
+  width: 100vw;
+  height: 100vh;
+`;
+
+function App(): JSX.Element {
+  return (
+    <Background>
+      <Selector />
+      <Ball number={3} />
+    </Background>
+  );
+}
+
+export default App;
